beabd7c924cbb8d96666b18668f0dc5f48266c9c
daregoodness1997 <daregoodness@gmail.com>
2022-06-25 19:40:19 +0100

    initial commit

---
+++ src/ui-components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { EuiButton } from '@elastic/eui';
+
+const Button = () => {
+  return (
+    <EuiButton color='primary' size='m' fill>
+      Label
+    </EuiButton>
+  );
+};
+
+export default Button;
